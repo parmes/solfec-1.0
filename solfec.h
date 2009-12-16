46f83e53fc1e7c1f0ddb9ab290b131a2925ca654
Tomasz Koziara <t.koziara@gmail.com>
2009-12-16 16:42:07 +0100

    Header solfec.h.

---
+++ solfec.h
@@ -0,0 +1,15 @@
+/*
+ * solfec.c
+ * Copyright (C) 2009, Tomasz Koziara (t.koziara AT gmail.com)
+ * --------------------------------------------------------------
+ * solfec main module
+ */
+
+#include "sol.h"
+
+#ifndef __solfec__
+#define __solfec__
+
+/* register new SOLFEC object */
+void REGISTER_SOLFEC (SOLFEC *sol);
+#endif
