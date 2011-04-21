704a5b868bb2f5e6989fa49b88fe3cd25d7e2888
Tomasz Koziara <t.koziara@gmail.com>
2011-04-21 17:19:15 +0100

    Abaqus reading initially done. Distance and angle measurment added to viewer.

--- inp/mbfcp/array_sin_sweep.py
+++ inp/mbfcp/array_sin_sweep.py
@@ -1,7 +1,48 @@
 # array of bricks
 import sys
+import commands
 sys.path.append ('inp/mesh')
 from abaqusread import *
+from math import sin
+
+dbgplt = 0 # enable debug plots
+
+PI = 3.14159265358979323846
+
+def gen_vel_sin_sweep (step):
+
+  f1 = 1.0
+  f2 = 15.0
+  Rlin = 0.3
+  A = 1.0
+  t2 = (f2 - f1) / Rlin
+  t = 0.0
+  velo = []
+  th = []
+  vh = []
+  c1 = PI*(f2-f1)/t2
+  c2 = 2*PI*f1
+
+  while t < t2:
+    # acceleration = A * cos (c1*t*t + c2*t)
+    v = A * sin (c1*t*t + c2*t) * (2*c1*t + c2)
+    velo.append (t)
+    velo.append (v)
+    if dbgplt:
+      th.append (t)
+      vh.append (v)
+    t = t + step
+
+  if dbgplt:
+    try:
+      import matplotlib.pyplot as plt
+      plt.plot (th, vh)
+      plt.xlabel ('Time')
+      plt.ylabel ('Velocity')
+      plt.savefig ('out/mbfcp/array_sin_sweep/velo.eps')
+    except: pass
+
+  return (t2, velo)
 
 #import rpdb2; rpdb2.start_embedded_debugger ('a')
 
@@ -9,6 +50,103 @@ step = 1E-3
 
 solfec = SOLFEC ('DYNAMIC', step, 'out/mbfcp/array_sin_sweep')
 
-ABAQUS_READ ('/Users/tomek/Desktop/A1.inp', solfec)
+SURFACE_MATERIAL (solfec, model = 'SIGNORINI_COULOMB', friction = 0.1)
 
-SURFACE_MATERIAL (solfec, model = 'SIGNORINI_COULOMB', friction = 0.1, restitution = 0.0)
+commands.getoutput ("bunzip2 inp/mesh/A1.inp.bz2")
+
+ABAQUS_READ ('inp/mesh/A1.inp', solfec)
+
+commands.getoutput ("bzip2 inp/mesh/A1.inp")
+
+gen = gen_vel_sin_sweep (step)
+stop = gen [0]
+vel = TIME_SERIES (gen [1])
+
+# boundary conditions
+for b in solfec.bodies:
+  c = b.center
+  t = b.tensor
+  if b.label == 'FBH':
+    p = TRANSLATE (c, (0.1, -0.1, 0))
+    FIX_DIRECTION (b, p, (0, 0, -1))
+    p = TRANSLATE (c, (-0.1, -0.1, 0))
+    FIX_DIRECTION (b, p, (0, 0, -1))
+    p = TRANSLATE (c, (0, 0.15, 0))
+    FIX_DIRECTION (b, p, (0, 0, -1))
+  elif b.label == 'IBH':
+    if abs (t [0]-t[4]) < 1E-6: # symmetrical case
+      p = TRANSLATE (c, (0.08, -0.08, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (-0.08, -0.08, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (0, 0.1, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+    else: # half keys
+      if c [0] < 0.0:
+	p = TRANSLATE (c, (0.05, 0, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (-0.04, 0.08, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	FIX_DIRECTION (b, p, (-1, 0, 0))
+	p = TRANSLATE (c, (-0.04, -0.08, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	FIX_DIRECTION (b, p, (-1, 0, 0))
+	SET_VELOCITY (b, p, (0, 1, 0), vel)
+      elif c [0] > 1.0:
+	p = TRANSLATE (c, (-0.05, 0, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (0.04, 0.08, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	FIX_DIRECTION (b, p, (1, 0, 0))
+	p = TRANSLATE (c, (0.04, -0.08, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	FIX_DIRECTION (b, p, (1, 0, 0))
+      elif c [1] < 0.0:
+	p = TRANSLATE (c, (0, 0.05, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (0.08, -0.04, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (-0.08, -0.04, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+      else:
+	p = TRANSLATE (c, (0, -0.05, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (0.08, 0.04, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+	p = TRANSLATE (c, (-0.08, 0.04, 0))
+	FIX_DIRECTION (b, p, (0, 0, -1))
+  elif b.label == 'LKH':
+    if t [0] < 0.51 * t [4]: # key along x
+      p = TRANSLATE (c, (0.0369, 0.016, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (-0.0369, 0.016, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (0, -0.01785, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+    elif t [4] < 0.51 * t [0]: # key along y
+      p = TRANSLATE (c, (0.016, 0.0369, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (0.016, -0.0369, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (-0.01785, 0, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+    else: # rectangular keys
+      p = TRANSLATE (c, (0.016, 0.016, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      p = TRANSLATE (c, (-0.016, 0.016, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      if c [0] < 0.0: FIX_DIRECTION (b, p, (-1, 0, 0))
+      p = TRANSLATE (c, (0, -0.01785, 0))
+      if c [0] < 0.0: FIX_DIRECTION (b, p, (-1, 0, 0))
+      FIX_DIRECTION (b, p, (0, 0, -1))
+      SET_VELOCITY (b, p, (0, 1, 0), vel)
+
+
+# solver and run
+
+slv  = GAUSS_SEIDEL_SOLVER (1E-3, 300, 1E-6)
+
+if RANK() == 0 and solfec.mode == 'WRITE':
+  print 'Running', stop, 'seconds or analysis with step', step, '...'
+
+RUN (solfec, slv, stop)
