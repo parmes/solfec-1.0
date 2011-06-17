82796e65f7e748dd4d111add47947662a72695d0
Tomasz Koziara <t.koziara@gmail.com>
2011-06-17 15:30:45 +0100

    Birck impact example modified.

--- inp/fuel-brick-impact.py
+++ inp/fuel-brick-impact.py
@@ -62,66 +62,64 @@ def scene_run (step, stop, kinem, formul, damp):
 
 stop = 0.8
 pairs = []
-pairs.append (scene_run (0.001, stop, 'FINITE_ELEMENT', 'BC', 0))
-pairs.append (scene_run (0.001, stop, 'FINITE_ELEMENT', 'BC', 1E-5))
-pairs.append (scene_run (0.001, stop, 'FINITE_ELEMENT', 'BC', 1E-4))
-pairs.append (scene_run (0.0009, stop, 'FINITE_ELEMENT', 'BC', 0))
-pairs.append (scene_run (0.0009, stop, 'FINITE_ELEMENT', 'BC', 1E-5))
-pairs.append (scene_run (0.0009, stop, 'FINITE_ELEMENT', 'BC', 1E-4))
+pairs.append (scene_run (0.0010, stop, 'FINITE_ELEMENT', 'BC', 0))
 pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 0))
-pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 1E-5))
-pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 1E-4))
+pairs.append (scene_run (0.0002, stop, 'FINITE_ELEMENT', 'BC', 0))
 pairs.append (scene_run (0.0001, stop, 'FINITE_ELEMENT', 'BC', 0))
-pairs.append (scene_run (0.0001, stop, 'FINITE_ELEMENT', 'BC', 1E-5))
-pairs.append (scene_run (0.0001, stop, 'FINITE_ELEMENT', 'BC', 1E-4))
 
-def makeplot (step, iter):
+pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 1E-5))
+pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 5E-5))
+pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 1E-4))
+pairs.append (scene_run (0.0005, stop, 'FINITE_ELEMENT', 'BC', 1E-3))
+
+if not VIEWER() and pairs [0][0].mode == 'READ':
+
   try:
     import matplotlib.pyplot as plt
 
     d = []
-    h = []
-    for i in range (iter, iter+3):
+    for i in range (0, 4):
       solfec = pairs [i][0]
       b = BYLABEL (solfec, 'BODY', 'FU')
-      p0 = (0.1315, 0, 1)
-      p1 = (0.1315, 0, 1.45)
-      th = HISTORY (solfec, [(b, p0, 'DZ'), (b, p1, 'DZ')], 0.3, 0.8)
-
-      d1 = []
-      for j in range (0, len (th[0])):
-	z1 = p0[2] + th [1][j]
-	z2 = p1[2] + th [2][j]
-	dz = z2 - z1
-	d1.append (dz)
-
-      d.append (d1)
-      h.append (th[2])
+      pnt = (0.1315, 0, 1.45)
+      th = HISTORY (solfec, [(b, pnt, 'DZ')], 0.3, 0.8)
+      d.append (th[0])
+      d.append (th[1])
 
     plt.clf ()
-    plt.title ( 'Step ' + str (step) + ', CR-FE')
-    plt.plot (th [0], d[0], label = '$\eta=0$')
-    plt.plot (th [0], d[1], label = '$\eta=10^{-5}$')
-    plt.plot (th [0], d[2], label = '$\eta=10^{-4}$')
-    plt.xlabel ('Time [s]')
-    plt.ylabel ('Brick height [m]')
-    plt.legend(loc = 'lower left')
-    plt.savefig ('out/fuel-brick-impact/fb_hh_step' + str(step) + '_eta1.eps')
-    plt.clf ()
-    plt.title ( 'Step ' + str (step) + ', CR-FE')
-    plt.plot (th [0], h[0], label = '$\eta=0$')
-    plt.plot (th [0], h[1], label = '$\eta=10^{-5}$')
-    plt.plot (th [0], h[2], label = '$\eta=10^{-4}$')
+    plt.title ( 'Various time steps, CR-FE')
+    plt.plot (d[0], d[1], label = '$h=1^{-3}$')
+    plt.plot (d[2], d[3], label = '$h=5^{-4}$')
+    plt.plot (d[4], d[5], label = '$h=2^{-4}$')
+    plt.plot (d[6], d[7], label = '$h=2^{-4}$')
     plt.xlabel ('Time [s]')
     plt.ylabel ('Displacement DZ [m]')
     plt.legend(loc = 'lower left')
-    plt.savefig ('out/fuel-brick-impact/fb_dz_step' + str(step) + '_eta1.eps')
+    plt.savefig ('out/fuel-brick-impact/fb_dz_varstep.eps')
   except ImportError:
     pass # no reaction
 
-if not VIEWER() and pairs [0][0].mode == 'READ':
+  try:
+    import matplotlib.pyplot as plt
 
-  makeplot (0.0010, 0)
-  makeplot (0.0009, 3)
-  makeplot (0.0005, 6)
-  makeplot (0.0001, 9)
+    d = []
+    for i in (1, 4, 5, 6, 7):
+      solfec = pairs [i][0]
+      b = BYLABEL (solfec, 'BODY', 'FU')
+      pnt = (0.1315, 0, 1.45)
+      th = HISTORY (solfec, [(b, pnt, 'DZ')], 0.3, 0.8)
+      d.append (th[1])
+
+    plt.clf ()
+    plt.title ( 'Step 0.0005, CR-FE')
+    plt.plot (th [0], d[0], label = '$\eta=0$')
+    plt.plot (th [0], d[1], label = '$\eta=1^{-5}$')
+    plt.plot (th [0], d[2], label = '$\eta=5^{-5}$')
+    plt.plot (th [0], d[3], label = '$\eta=1^{-4}$')
+    plt.plot (th [0], d[4], label = '$\eta=1^{-3}$')
+    plt.xlabel ('Time [s]')
+    plt.ylabel ('Displacement DZ [m]')
+    plt.legend(loc = 'lower left')
+    plt.savefig ('out/fuel-brick-impact/fb_dz_vareta.eps')
+  except ImportError:
+    pass # no reaction
