850dffa35a4262b92016f4f050947fd056d7a48d
Tomasz Koziara <t.koziara@gmail.com>
2012-01-16 16:06:36 +0000

    RO bugfixing and smoothing.

--- inp/ro.py
+++ inp/ro.py
@@ -16,7 +16,6 @@ nodes = [-a, -a, -l*a,
          -a,  a,  l*a]
 
 mesh = HEX (nodes, n, n, l*n, 0, [0, 1, 2, 3, 4, 5])
-nod = mesh.node (0)
 
 print 'Computing ', m, 'eigenpairs of a ', 3 * mesh.nnod, 'system ...'
 
@@ -28,15 +27,23 @@ bulk = BULK_MATERIAL (solfec,
 		      poisson = 0.2,
 		      density = 2E3)
 
-bod = BODY (solfec, 'FINITE_ELEMENT', mesh, bulk, form = 'RO')
+bod = BODY (solfec, 'FINITE_ELEMENT', COPY (mesh), bulk, form = 'RO')
 bod.scheme = 'DEF_LIM'
-
 MODAL_ANALYSIS (bod, m)
+INITIAL_VELOCITY (bod, (0, 0, 0), (1, 0, 0))
+
+for i in range (1, 5): # run -np 4 to get malloc errors
+  if 1:
+    bod = BODY (solfec, 'FINITE_ELEMENT', TRANSLATE (COPY (mesh), (0, i, 0)), bulk, form = 'RO')
+    bod.scheme = 'DEF_LIM'
+    MODAL_ANALYSIS (bod, m)
+    INITIAL_VELOCITY (bod, (0, 0, 0), (1, 0, 0))
+  else:
+    b = CLONE (bod, (0, i, 0))
+    INITIAL_VELOCITY (b, (0, 0, 0), (1, 0, 0))
 
 ns = NEWTON_SOLVER ()
 
 #GRAVITY (solfec, (0, 0, -10))
-
-INITIAL_VELOCITY (bod, (0, 0, 0), (1, 0, 0))
 
 RUN (solfec, ns, 10)
