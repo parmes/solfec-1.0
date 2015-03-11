cdd3cf2a361b115e4b08446938b8689096277a1c
Tomasz Koziara <t.koziara@gmail.com>
2015-03-11 17:09:47 +0000

    Costy - rotation mitigation code placeholder.

---
+++ rbmm.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+
+#include <assert.h> 
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <string.h>
+
+extern "C"
+{
+#include "sol.h"
+#include "dom.h"
+#include "msh.h"
+}
+
+#define PI 3.14159265358979323846
