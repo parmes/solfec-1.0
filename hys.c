b6511af196aea54d8f73ad7a1a233140c6372270
Tomasz Koziara <t.koziara@gmail.com>
2017-03-08 12:37:16 +0100

    MPI version of the hybrid solver started

--- hys.c
+++ hys.c
@@ -37,6 +37,123 @@ SOFTWARE.
 #include "alg.h"
 #include "err.h"
 
+#if MPI
+#include <mpi.h>
+#endif
+
+#if MPI
+/* initialize boudary */
+static void init_boundary (HYBRID_SOLVER *hs, DOM *dom)
+{
+  int rank, size, *sendbuf, sendcount, *recvbuf, recvsize, *recvcounts, *displs, *ptr;
+  MAP *found, *item;
+
+  for (found = NULL, item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item))
+  {
+    BODY *bod = MAP_Find (dom->idb, item->key, NULL);
+
+    if (bod)
+    {
+      MAP_Insert (NULL, &found, item->key, NULL, NULL);
+
+      if (!bod->parmec) ERRMEM (bod->parmec = MEM_CALLOC (sizeof(PARMEC_FORCE))); /* allocate parmec force */
+    }
+  }
+
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+  sendcount = MAP_Size(found);
+  recvsize = MAP_Size(hs->solfec2parmec) * size;
+  ERRMEM (sendbuf = malloc (sendcount * sizeof(int)));
+  if (rank == 0)
+  {
+    ERRMEM (recvbuf = malloc (recvsize * sizeof(int)));
+    ERRMEM (recvcounts = MEM_CALLOC(size * sizeof(int)));
+    ERRMEM (displs = MEM_CALLOC(size * sizeof(int)));
+  }
+
+  for (item = MAP_First(found), ptr = sendbuf; item; item = MAP_Next(item), ptr ++)
+  {
+    ptr[0] = (int) (long) item->key;
+  }
+
+  MPI_Gatherv (sendbuf, sendcount, MPI_INT, recvbuf, recvcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (rank == 0)
+  {
+    MAP_Free (NULL, &found);
+
+    for (int i = 0; i < size; i ++)
+    {
+      for (int j = 0; j < recvcounts[i]; j ++)
+      {
+	MAP_Insert (NULL, &found, (void*) (long) recvbuf[displs[i]+j], NULL, NULL);
+      }
+    }
+
+    for (MAP *item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item)) /* make rank-wide consistency check */
+    {
+      BODY *bod = MAP_Find (found, item->key, NULL);
+      ASSERT_TEXT (bod, "ERROR: Solfec-Parmec boundary body with Solfec id = %d has not been found", (int)(long) item->key);
+      ASSERT_TEXT (bod->kind == RIG, "ERROR: Solfec-Parmec boundary body with Solfec id = %d is not rigid", (int)(long) item->key);
+    }
+
+    free (recvbuf);
+    free (recvcounts);
+    free (displs);
+  }
+
+  free (sendbuf);
+  MAP_Free (NULL, &found);
+}
+
+/* apply constant boundary force from Solfec;
+ * perform a number of Parmec time integration steps;
+ * read back average velocity of boundary bodies into Solfec */
+static void parmec_steps (HYBRID_SOLVER *hs, DOM *dom, double step, int nstep)
+{
+  MAP *item;
+
+  /* XXX --> the below code needs to be MPI-ed */
+  ASSERT_TEXT (0, "Ooops --> encountered unimlpemented aspect!");
+
+  for (item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item))
+  {
+    double force[3], torque[3];
+    BODY *bod = MAP_Find (dom->idb, item->key, NULL);
+    BODY_Rigid_Force (bod, dom->time, dom->step, force, torque);
+    int num = (int) (long) item->data; /* parmec particle number */
+    parmec_set_force_and_torque (num, force, torque);
+    SET6 (bod->velo, 0.0);
+  }
+
+  for (int i = 0; i < nstep; i ++)
+  {
+    parmec_one_step (step, hs->parmec_interval, hs->parmec_prefix);
+
+    for (item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item))
+    {
+      BODY *bod = MAP_Find (dom->idb, item->key, NULL);
+      int num = (int) (long) item->data; /* parmec particle number */
+      double angular[3], linear[3];
+      parmec_get_angular_and_linear (num, angular, linear);
+      ACC (angular, bod->velo);
+      ACC (linear, bod->velo+3);
+    }
+  }
+
+  double inv = 1.0/(double)nstep;
+
+  for (item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item))
+  {
+    BODY *bod = MAP_Find (dom->idb, item->key, NULL);
+    int num = (int) (long) item->data; /* parmec particle number */
+    parmec_get_force_and_torque (num, nstep, bod->parmec->force, bod->parmec->torque);
+    SCALE6 (bod->velo, inv);
+  }
+}
+#else
 /* initialize boudary */
 static void init_boundary (HYBRID_SOLVER *hs, DOM *dom)
 {
@@ -91,6 +208,7 @@ static void parmec_steps (HYBRID_SOLVER *hs, DOM *dom, double step, int nstep)
     SCALE6 (bod->velo, inv);
   }
 }
+#endif
 
 /* create solver */
 HYBRID_SOLVER* HYBRID_SOLVER_Create (char *parmec_file, double parmec_step, double parmec_interval[2],
@@ -118,6 +236,11 @@ HYBRID_SOLVER* HYBRID_SOLVER_Create (char *parmec_file, double parmec_step, doub
   hs->solfec_solver = solfec_solver;
   hs->solfec_solver_kind = solfec_solver_kind;
 
+#if MPI
+  int rank;
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  if (rank == 0) /* parmec lib is used on rank 0 */
+#endif
   parmec_init(hs->parmec_file);
 
   /* TODO: perhaps invoke Solfec's python init so that Parmec's commands
@@ -179,6 +302,10 @@ void HYBRID_SOLVER_Run (HYBRID_SOLVER *hs, SOLFEC *sol, double duration)
   sol->duration = duration; /* in SOLFEC_Run, allowing for a correct elapsed time estimate */
   while (sol->dom->time < time0 + duration)
   {
+#if MPI
+    /* XXX --> we need to send rotation/position to their destination ranks */
+    ASSERT_TEXT (0, "Ooops --> encountered unimlpemented aspect!");
+#else
     for (MAP *item = MAP_First(hs->solfec2parmec); item; item = MAP_Next (item))
     {
       BODY *bod = MAP_Find (sol->dom->idb, item->key, NULL);
@@ -186,6 +313,7 @@ void HYBRID_SOLVER_Run (HYBRID_SOLVER *hs, SOLFEC *sol, double duration)
       parmec_get_rotation_and_position (num, bod->conf, bod->conf+9);
       SHAPE_Update (bod->shape, bod, (MOTION)BODY_Cur_Point);
     }
+#endif
 
     SOLFEC_Run (sol, hs->solfec_solver_kind, hs->solfec_solver, -sol->dom->step); /* negative duration used */
 
@@ -204,8 +332,6 @@ void HYBRID_SOLVER_Run (HYBRID_SOLVER *hs, SOLFEC *sol, double duration)
       }
     }
   }
-
-  /* TODO --> MPI */
 }
 
 /* destroy solver */
