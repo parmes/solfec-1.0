015201d7e26ba007982e90f0baf9a1b9850f58e9
Tomasz Koziara <t.koziara@gmail.com>
2017-03-21 17:11:13 +0100

    HSE0 edits

--- examples/hybrid-solver0/hs0-solfec-2.py
+++ examples/hybrid-solver0/hs0-solfec-2.py
@@ -1,4 +1,4 @@
-# same as in hs0-solfec-1.py
+# as in hs0-solfec-1.py
 step = 0.01
 sol = SOLFEC ('DYNAMIC', step, 'out/hybrid-solver0')
 GRAVITY (sol, (0, 0, -10))
@@ -12,6 +12,9 @@ TRANSLATE (msh, (0, 0, 1.1))
 bod2 = BODY (sol, 'RIGID', msh, mat)
 ns = NEWTON_SOLVER ()
 
+# simulation end time
+stop = 10.0
+
 # parmec's output files are written to the same location as the input path;
 # for that to be the solfec's output directory, we copy parmec's input file there
 from shutil import copyfile
@@ -22,7 +25,7 @@ copyfile('examples/hybrid-solver0/hs0-parmec.py', 'out/hybrid-solver0/hs0-parmec
 hs = HYBRID_SOLVER ('out/hybrid-solver0/hs0-parmec.py', step, {0 : 1}, ns)
 
 import parmec as parmec # parmec module becomes available after creation of the HYBRID_SOLVER
-tms0 = parmec.TSERIES ([0, 0.03, 5, 0.03, 5+step, 0.1, 10, 0.1])
+tms0 = parmec.TSERIES ([0, 0.03, 5, 0.03, 5+step, 0.1, stop, 0.1])
 hs.parmec_interval = (tms0, 0.03) # variable output file interval and constant output history interval
 
 # parmec time history plots are collected at runtime
@@ -43,7 +46,7 @@ import solfec as solfec # we need to be specific when using the OUTPUT command
 solfec.OUTPUT (sol, 0.03) # since 'OUTPUT' in Solfec collides with 'OUTPUT' in Parmec
 
 # run simulation
-RUN (sol, hs, 10)
+RUN (sol, hs, stop)
 
 # plot time histories
 try:
@@ -53,9 +56,15 @@ try:
   plt.plot (t_parmec, dz_parmec, label = 'lower body (parmec)')
   plt.plot (t_solfec, dz_solfec, label = 'upper body (solfec)')
   plt.legend (loc = 'upper right')
-  plt.xlim ((0, 10))
+  plt.xlim ((0, stop))
   plt.xlabel ('time $(s)$')
   plt.ylabel ('dz $(m)$')
   plt.savefig ('out/hybrid-solver0/hs0-dz.png')
 except:
   print 'Plotting using matplotlib has failed.'
+
+# XDMF export
+if sol.mode == 'WRITE' and not VIEWER():
+  print 'Run one more times to export XDMF files!'
+elif sol.mode == 'READ' and not VIEWER():
+  XDMF_EXPORT (sol, (0.0, stop), 'out/hybrid-solver0/hs0-solfec', [3, 4, 5])
