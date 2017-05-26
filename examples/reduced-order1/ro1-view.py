d635a1bd8fd5a83045df65b7194f600e039313a6
Tomasz Koziara <t.koziara@gmail.com>
2017-05-26 13:34:01 +0200

    ro1-view updated

--- examples/reduced-order1/ro1-view.py
+++ examples/reduced-order1/ro1-view.py
@@ -1,10 +1,21 @@
 import os, sys
 dirpath = os.path.dirname(os.path.realpath(__file__))
-# juxtapose three simulations in the viewer
-percentage = True
-print 'Reading/running FEM-TL model ...',
-execfile (dirpath + '/ro1-fem-tl.py')
-print 'Reading/running FEM-BC model ...',
-execfile (dirpath + '/ro1-fem-bc.py')
-print 'Reading/running BC-RO model ...',
-execfile (dirpath + '/ro1-reduced.py')
+execfile (dirpath + '/ro1-lib.py') # import library
+
+# read simulations
+(sol_tl, bod_tl, stop_tl) = ro1_simulation ('fem-tl')
+if sol_tl.mode == 'WRITE': print 'Results all TL simulation are not present'
+
+(sol_bc, bod_bc, stop_bc) = ro1_simulation ('fem-bc')
+if sol_bc.mode == 'WRITE': print 'Results all BC simulation are not present'
+
+sol_ro_mode = 'WRITE'
+if sol_tl.mode == 'READ':
+  (sol_ro, bod_ro, stop_ro) = ro1_simulation ('reduced')
+  sol_ro_mode = sol_ro.mode
+  if sol_ro.mode == 'WRITE': print 'Results all BC-RO simulation are not present'
+
+if sol_tl.mode == 'WRITE' or sol_bc.mode == 'WRITE' or sol_ro_mode == 'WRITE':
+  print 'Run first --> solfec exaples/reduced-order1/ro1-run-all.py'
+  print 'Run next --> solfec -v exaples/reduced-order1/ro1-view.py'
+  sys.exit(0)
