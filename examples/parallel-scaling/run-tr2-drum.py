841200b35c9b136e8ef4281d4cc53aed5560d6a9
Tomasz Koziara <t.koziara@gmail.com>
2017-11-04 14:47:47 +0100

    parallel-scaling moved to examples

--- examples/parallel-scaling/run-tr2-drum.py
+++ examples/parallel-scaling/run-tr2-drum.py
@@ -1,3 +1,4 @@
+# auxiliary serial multi-running script
 import subprocess
 import os
 dirpath = os.path.dirname(os.path.realpath(__file__))
@@ -19,7 +20,7 @@ argv = [dirpath+'/rotating-drum.py -kifo RG -nsdl 0.0',
 
 argv = [dirpath+'/rotating-drum.py -kifo RG -nsep 0.75',
         dirpath+'/rotating-drum.py -kifo RG -nsep 0.5',
-        #dirpath+'/rotating-drum.py -kifo RG -nsep 0.25',
+        dirpath+'/rotating-drum.py -kifo RG -nsep 0.25',
         dirpath+'/rotating-drum.py -kifo RG -nsep 0.2',
         dirpath+'/rotating-drum.py -kifo RG -nsep 0.1',
         dirpath+'/rotating-drum.py -kifo RG -nsep 0.05']
