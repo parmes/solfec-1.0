39342da3b64ba1c9c5971db3c0fc1f75d82c3e69
Tomasz Koziara <t.koziara@gmail.com>
2019-04-05 11:49:26 +0200

    BM05 test added

--- tests/serial-tests.py
+++ tests/serial-tests.py
@@ -15,6 +15,7 @@ tests = [
 	 'tests/BM03/BM03_tetra.py',
 	 'tests/BM04/BM04_hexa.py',
 	 'tests/BM04/BM04_tetra.py',
+	 'tests/BM05/BM05_hexa.py',
 	 'tests/BM06/BM06_hexa.py',
 	 'tests/BM07/BM07_hexa.py',
 	 'tests/BM08/BM08_fine.py',
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION_HASH "24e88bd"
-#define VERSION_DATE "2019-03-05"
+#define VERSION_HASH "69261ba"
+#define VERSION_DATE "2019-03-28"
